3a3fc0d2e5e7d99361e49a4a819cfb03a40aac7a
fiorotticaio <caiofiorotti@gmail.com>
2022-03-04 18:15:04 -0300

    proximo passo eh fazer a plataforma infinita

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,25 +9,36 @@ export default class Game extends Phaser.Scene {
 
 
     init() {
-        this.pontuacao = 0
+        this.pontuation = 0;
     }
 
 
     preload() {
         /* fazendo o carregamento das imagens */
-        this.load.image('background', './src/sprites/background.png');
+        this.load.image('background', './src/sprites/backgroundCastles.png');
         this.load.image('dino', './src/sprites/dino.png');
+        this.load.image('dinoJump', './src/sprites/dinoJump.png');
+        this.load.image('platform', './src/sprites/platform.png');
+
+        /* incluindo os cursores */
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
     
 
     create() {
         /* adcionando as imagens carregadas */
-        this.add.image(512, 512, 'background');
-        this.add.image(200, 650, 'dino').setScale(0.5);
+        this.add.image(512, 512, 'background').setScrollFactor(0); // não se move
+
+        this.dino = this.physics.add.sprite(200, 680, 'dino').setScale(0.5);
+
+        this.platform = this.physics.add.sprite(430, 830, 'platform').setImmovable(true);
+        this.platform.body.setAllowGravity(false); // impedir que a plataforma caia
+
+        // this.physics.add.collider(this.platform, this.dino);
 
         /* texto da pontuação */
         const style = { color: '#000', fontSize: 24 };
-        this.textoPontuacao = this.add.text(800, 10, 'Pontuação: 0', style)
+        this.textPontuation = this.add.text(800, 10, 'Pontuation: 0', style)
             .setScrollFactor(0)
             .setOrigin(0.5, 0)
         ;
@@ -35,12 +46,35 @@ export default class Game extends Phaser.Scene {
 
     
     update() {
-        this.handlePontuacao();
+        this.handlePontuation();
+
+        this.platform.setVelocityX(-200);
+        
+        if(this.cursors.up.isDown && this.dino.body.position.y > 577 && this.dino.body.position.y < 584) {
+            this.dino.setVelocityY(-500);
+            this.dino.setTexture('dinoJump');
+        } else {
+            this.dino.setVelocityX(0);
+        }
+        
+        console.log(this.dino.body.position.y);
+        
+        /* cancela a gravidade quando chega na altura da plataforma */ 
+        if(this.dino.body.position.y > 583) {
+            this.dino.setImmovable(true);
+            this.dino.body.setAllowGravity(false);
+            this.dino.setVelocityY(-100);
+            this.dino.setTexture('dino');
+
+        } else {
+            this.dino.setImmovable(false);
+            this.dino.body.setAllowGravity(true);
+        }
     }
 
 
-    handlePontuacao() {
-        this.pontuacao++;
-        this.textoPontuacao.text = `Pontuação: ${this.pontuacao}`;
+    handlePontuation() {
+        this.pontuation++;
+        this.textPontuation.text = `Pontuação: ${this.pontuation}`;
     }
 }
--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -9,12 +9,12 @@ export default class Start extends Phaser.Scene {
 
     preload() {
         /* fazendo o carregamento das imagens */
-        this.load.image('background', './src/sprites/background.png');
-        this.load.image('dino', './src/sprites/dino.png');
-        this.load.image('botaoEspaco', './src/sprites/botaoEspaco.png');
+        this.load.image('background', './src/sprites/backgroundCastles.png');
+        this.load.image('dinoRun', './src/sprites/dinoRun.png');
+        this.load.image('spaceButton', './src/sprites/spaceButton.png');
 
         /* fazendo o carregamento dos sons */
-        this.load.audio('startMusic', './src/sprites/startMusic.mp3');
+        this.load.audio('startMusic', './src/sounds/startMusic.mp3');
     }
 
 
@@ -24,8 +24,8 @@ export default class Start extends Phaser.Scene {
 
         /* adcionando as imagens carregadas */
         this.add.image(512, 512, 'background');
-        this.add.image(200, 650, 'dino').setScale(0.5);
-        this.add.image(width*0.5, (height*0.5)-200, 'botaoEspaco').setScale(1.2); // fazer piscar
+        this.add.image(550, 550, 'dinoRun').setScale(0.5);
+        this.button = this.add.image(width*0.5, (height*0.5)-200, 'spaceButton').setScale(1.2); // fazer piscar
 
         /* adicionando a musica inicial */
         this.sound.play('startMusic');
@@ -35,7 +35,7 @@ export default class Start extends Phaser.Scene {
             fontSize: 48
         };
         /* adicionando a frase na tela */
-        this.add.text(width*0.5, (height*0.5)-300, 'Pressione ESPAÇO para iniciar', style)
+        this.add.text(width*0.5, (height*0.5)-300, 'Press SPACE to play', style)
             .setOrigin(0.5)
         ;
 
@@ -44,5 +44,19 @@ export default class Start extends Phaser.Scene {
             this.sound.stopAll(); // parar a música inicial
             this.scene.start('game');
         })
+
+        /* fazendo o botão diminuir e aumentar de tamanho */
+        setInterval(() => {
+            if(this.button.scale == 1.2) {
+                this.button.setScale(1.5);
+            } else {
+                this.button.setScale(1.2);
+            }
+        }, 400);
+    }
+
+
+    update() {
+     
     }
 }
